ef847cf1ed0181e72c1d9e10f083ba5653d09bc7
SvetaLantsman <svetlana@curalife.com>
2023-05-20 20:52:20 +0300

    .

--- src/components/SomeInput.js
+++ src/components/SomeInput.js
@@ -1,52 +1,42 @@
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
 const SomeInput = (props) => {
-  const nameInputRef = useRef();
+  
   const [enteredName, setEnteredName] = useState("");
-  const [isEnteredNameValid, setIsEnteredNameValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  useEffect(() => {
-    if (isEnteredNameValid) {
-      console.log("Name Input is Valid!");
-    }
-  }, [isEnteredNameValid]);
+  const isEnteredNameValid = enteredName.trim() !== "";
+  const isNameInputInvalid = !isEnteredNameValid && enteredNameTouched;
+
+  
   const nameInputChangedHandler = (event) => {
     setEnteredName(event.target.value);
-    if (event.target.value.trim() !== "") {
-      setIsEnteredNameValid(true);
-    }
+    
   };
 
   const inputLostFocusHandler = (event) => {
     setEnteredNameTouched(true);
-   if (enteredName.trim() === "") {
-      setIsEnteredNameValid(false);
-      return;
-    }
+  
   }
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    setEnteredNameTouched(true);
-    if (enteredName.trim() === "") {
-      setIsEnteredNameValid(false);
+    setEnteredNameTouched(true); 
+    if (!isEnteredNameValid) {
       return;
     }
-
-    setIsEnteredNameValid(true);
-    console.log(enteredName);
-    console.log(nameInputRef.current.value);
+    console.log(enteredName);   
     setEnteredName("");
-    //  nameInputRef.current.value = ""; // DON'T DO THIS - REACT WON'T LIKE IT
+    setEnteredNameTouched(false);
+   
   };
-
-  const isNameInputInvalid = !isEnteredNameValid && enteredNameTouched;
+ 
   const nameInputClasses = isNameInputInvalid ? "form-control invalid" : "form-control";
+
   return (
     <form onSubmit={formSubmitHandler}>
       <div className={nameInputClasses}>
         <label htmlFor="name">Введите Имя</label>
-        <input ref={nameInputRef} onBlur={inputLostFocusHandler} onChange={nameInputChangedHandler} type="text" id="name" value={enteredName}/>
+        <input onBlur={inputLostFocusHandler} onChange={nameInputChangedHandler} type="text" id="name" value={enteredName}/>
         {isNameInputInvalid && <p className="error-text">Имя не может быть пустым</p>}
       </div>
       <div className="form-actions">
