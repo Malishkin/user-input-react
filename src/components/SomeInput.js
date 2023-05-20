5ce52a5477522c2275ab6afbd1f1cc20975b4c68
SvetaLantsman <svetlana@curalife.com>
2023-05-20 17:45:04 +0300

    .

--- src/components/SomeInput.js
+++ src/components/SomeInput.js
@@ -1,9 +1,23 @@
+import { useState, useRef } from "react";
 const SomeInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const nameInputChangedHandler = (event) => {
+    setEnteredName(event.target.value);
+    }
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+    console.log(nameInputRef.current.value);
+    setEnteredName("");
+    //  nameInputRef.current.value = ""; // DON'T DO THIS - REACT WON'T LIKE IT
+  };
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label htmlFor="name">Введите Имя</label>
-        <input type="text" id="name" />
+        <input ref={nameInputRef} onChange={nameInputChangedHandler} type="text" id="name" value={enteredName}/>
       </div>
       <div className="form-actions">
         <button>Отправить</button>
