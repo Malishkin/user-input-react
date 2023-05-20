5a5e27b8412c0a0586ff5630efe09da888f2c487
SvetaLantsman <svetlana@curalife.com>
2023-05-20 18:49:08 +0300

    .

--- src/components/SomeInput.js
+++ src/components/SomeInput.js
@@ -1,23 +1,53 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 const SomeInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
+  const [isEnteredNameValid, setIsEnteredNameValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  useEffect(() => {
+    if (isEnteredNameValid) {
+      console.log("Name Input is Valid!");
+    }
+  }, [isEnteredNameValid]);
   const nameInputChangedHandler = (event) => {
     setEnteredName(event.target.value);
+    if (event.target.value.trim() !== "") {
+      setIsEnteredNameValid(true);
     }
+  };
+
+  const inputLostFocusHandler = (event) => {
+    setEnteredNameTouched(true);
+   if (enteredName.trim() === "") {
+      setIsEnteredNameValid(false);
+      return;
+    }
+  }
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    setEnteredNameTouched(true);
+    if (enteredName.trim() === "") {
+      setIsEnteredNameValid(false);
+      return;
+    }
+
+    setIsEnteredNameValid(true);
     console.log(enteredName);
     console.log(nameInputRef.current.value);
     setEnteredName("");
     //  nameInputRef.current.value = ""; // DON'T DO THIS - REACT WON'T LIKE IT
   };
+
+  const isNameInputInvalid = !isEnteredNameValid && enteredNameTouched;
+  const nameInputClasses = isNameInputInvalid ? "form-control invalid" : "form-control";
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Введите Имя</label>
-        <input ref={nameInputRef} onChange={nameInputChangedHandler} type="text" id="name" value={enteredName}/>
+        <input ref={nameInputRef} onBlur={inputLostFocusHandler} onChange={nameInputChangedHandler} type="text" id="name" value={enteredName}/>
+        {isNameInputInvalid && <p className="error-text">Имя не может быть пустым</p>}
       </div>
       <div className="form-actions">
         <button>Отправить</button>
